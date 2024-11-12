e8841dfa33e4bc1084d1b9ce0d2863703b64136e
sbumilix <sbumilix@gmail.com>
2024-11-12 16:42:07 +0300

    myau

--- Program.cs
+++ Program.cs
@@ -69,6 +69,10 @@ namespace ConsoleApp2
             }
         }
 
+        public void myau() {
+            Console.Write("Мяу");
+        }
+
         // вывод матрицы на экран
         public void output()
         {
